212af2061e0bcc03ee840a0fb66c3983d036ed5a
Michal Mielewczyk <michal.mielewczyk10@gmail.com>
2025-12-24 00:31:21 +0100

    Remove radix

--- serial/radix.h
+++
@@ -1,3 +0,0 @@
-
-void radix_tree_insert(struct radix_tree_node *node, char *key, uint32_t key_len);
-struct radix_tree_node *radix_tree_find(struct radix_tree_node *node, char *key, uint32_t key_len);
--- tests/serial_b/test_serial.c
+++ tests/serial_b/test_serial.c
@@ -1,4 +1,4 @@
-#include "radix.c"
+#include "hashtable.c"
 #include "unity.h"
 #include <string.h>
 
@@ -10,28 +10,9 @@ void tearDown(void)
 {
 }
 
-void test_radix_insert_1(void)
-{
-    struct radix_tree_node root = {0};
-    const char key[] = "abcd1234";
-    uint32_t key_len = (uint32_t)strlen(key);
-
-    strncpy(root.key, key, RADIX_KEY_LEN);
-    root.key_len = (uint8_t)key_len;
-
-    radix_tree_insert(&root, (char *)key, key_len);
-
-    TEST_ASSERT_EQUAL_UINT32(1, root.count);
-
-    struct radix_tree_node *found = radix_tree_find(&root, (char *)key, key_len);
-    TEST_ASSERT_NOT_NULL(found);
-    TEST_ASSERT_EQUAL_PTR(&root, found);
-    TEST_ASSERT_EQUAL_UINT32(1, found->count);
-}
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_radix_insert_1);
 
     return UNITY_END();
 }
