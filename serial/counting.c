d102561f5455d3e541942f8b7fbd76021531acd0
Michal Mielewczyk <michal.mielewczyk10@gmail.com>
2025-12-25 12:51:17 +0100

    Insert models into the hashtable

--- serial/counting.c
+++ serial/counting.c
@@ -7,6 +7,7 @@
 #include <pthread.h>
 
 #include "hashtable.c"
+#include "parse_json.c"
 #include "hash.c"
 
 #define SHARD_COUNT 4ul
@@ -14,44 +15,53 @@
 // (2 << 28) * 4 bytes == 1G of preallocated memory
 
 struct counting_ctx {
+	uint32_t shards_count;
 	struct hash_table_shard *shards;
 	pthread_rwlock_t *locks;
 };
 
-int counting_init(struct counting_ctx *ctx, uint32_t shard_count, uint32_t max_hashes) {
-	int i;
+int counting_init(struct counting_ctx *ctx, uint32_t shards_count, uint32_t max_hashes) {
+	uint64_t init_shards_counter = 0;
+	uint64_t init_locks_counter = 0;
+	uint64_t i;
 	int ret;
-	uint32_t shard_range = MAX_HASHES / SHARD_COUNT;
+	uint32_t shard_range = max_hashes / shards_count;
 
-	ctx->shards = calloc(shard_count, sizeof(struct hash_table_shard));
+	ctx->shards = calloc(shards_count, sizeof(struct hash_table_shard));
 	assert(ctx->shards);
-	ctx->locks = calloc(shard_count, sizeof(pthread_rwlock_t));
+	ctx->locks = calloc(shards_count, sizeof(pthread_rwlock_t));
 	assert(ctx->locks);
 
-	for (i = 0; i < SHARD_COUNT - 1; i++) {
-		ret = hashtable_init(ctx->shards[i], i*shard_range, (i + 1) * shard_range);
-		if (ret != 0)
-			goto err;
+	ctx->shards_count = shards_count;
+
+	if (shards_count > 1) {
+		for (i = 0; i < shards_count - 1; i++, init_shards_counter++) {
+			ret = hashtable_init(&ctx->shards[i], i*shard_range, (i + 1) * shard_range, FNV);
+			if (ret != 0)
+				goto err;
+		}
 	}
-	i = SHARD_COUNT - 1;
-	ret = hashtable_init(ctx->shards[i], i*shard_range, MAX_HASHES);
+
+	i = shards_count - 1;
+	ret = hashtable_init(&ctx->shards[i], i*shard_range, max_hashes, FNV);
 	if (ret != 0)
 		goto err;
 
-	for (i = 0; i < SHARD_COUNT; i++) {
-		ret = pthread_rwlock_init(&ctx[i].lock, 0);
+	init_shards_counter++;
+
+	for (i = 0; i < shards_count; i++, init_locks_counter++) {
+		ret = pthread_rwlock_init(&ctx->locks[i], 0);
 		if (ret != 0)
 			goto err;
 	}
 
 	return 0;
+
 err:
-	for (i = 0; i < SHARD_COUNT; i++) {
-		if (ctx->shards[i] != NULL)
-			hashtable_deinit(ctx->shards[i]);
-		if (ctx->locks[i] != NULL)
-			assert(pthread_rwlock_destroy(&ctx[i].lock) == 0)
-	}
+	for (i = 0; i < init_shards_counter; i++)
+			hashtable_deinit(&ctx->shards[i]);
+	for (i = 0; i < init_locks_counter; i++)
+			assert(pthread_rwlock_destroy(&ctx->locks[i]) == 0);
 
 	free(ctx->shards);
 	free(ctx->locks);
@@ -60,14 +70,89 @@ err:
 }
 
 void counting_deinit(struct counting_ctx *ctx) {
-	int i;
+	uint32_t i;
 
-	for (i = 0; i < SHARD_COUNT; i++) {
-		hashtable_deinit(ctx->shards[i]);
-		assert(pthread_rwlock_destroy(&ctx[i].lock) == 0)
+	for (i = 0; i < ctx->shards_count; i++) {
+		hashtable_deinit(&ctx->shards[i]);
+		assert(pthread_rwlock_destroy(ctx[i].locks) == 0);
 	}
 
 	free(ctx->shards);
 	free(ctx->locks);
 }
+
+int counting_insert_model(struct counting_ctx *ctx, char* model) {
+	int res;
+	uint32_t hash;
+	uint32_t shard_id;
+	struct hash_table_entry* entry;
+
+	hash = FNV(model);
+	shard_id = hash % ctx->shards_count;
+
+	res = pthread_rwlock_rdlock(&ctx->locks[shard_id]);
+	assert(res == 0);
+
+	entry = hashtable_lookup(&ctx->shards[shard_id], model);
+	if (!entry) {
+		res = pthread_rwlock_unlock(&ctx->locks[shard_id]);
+		assert(res == 0);
+		goto insert_new_element;
+	}
+
+	atomic_fetch_add(&entry->count, 1);
+
+	res = pthread_rwlock_unlock(&ctx->locks[shard_id]);
+	assert(res == 0);
+
+	return 0;
+
+insert_new_element:
+	res = pthread_rwlock_wrlock(&ctx->locks[shard_id]);
+	assert(res == 0);
+
+	res = hashtable_insert(&ctx->shards[shard_id], model);
+	assert(res == 0); // TODO sometimes it's recoverable that the allocation failed 
+
+	res = pthread_rwlock_unlock(&ctx->locks[shard_id]);
+	assert(res == 0);
+
+	return res;
+}
+
+int counting_models(struct counting_ctx *ctx, char* buffer, uint32_t remaining_buffer_len) {
+	int res;
+	char* curr_model_start_pos;
+	char* curr_model_end_pos;
+	char* curr_json_entry_begining;
+
+	while (remaining_buffer_len > 0) {
+		curr_json_entry_begining = memchr(buffer, '{', remaining_buffer_len);
+		if (curr_json_entry_begining == NULL)
+			break;
+
+		remaining_buffer_len -= (curr_json_entry_begining - buffer);
+		buffer = curr_json_entry_begining;
+
+		curr_model_start_pos = json_get_next_model(buffer, remaining_buffer_len);
+		if (*curr_model_start_pos == '}')
+			break;
+
+		curr_model_end_pos = memchr(curr_model_start_pos, '\"', remaining_buffer_len);
+		assert(curr_model_end_pos != NULL); // The input must be valid JSON
+
+		*curr_model_end_pos = 0;
+		res = counting_insert_model(ctx, curr_model_start_pos);
+		assert(res == 0);
+
+		curr_model_end_pos++;
+
+		remaining_buffer_len -= (curr_model_end_pos - buffer);
+
+		buffer = curr_model_end_pos;
+	}
+
+	return res;
+}
+
 #endif
--- tests/serial_b/test_json.c
+++ tests/serial_b/test_json.c
@@ -95,15 +95,6 @@ void test_json_find_model_negative(void) {
 	TEST_ASSERT_EQUAL('}', *model_ptr);
 }
 
-/*
-void test_json_find_model_real_data(void) {
-	char input_buffer[] = {"{\"id\":0,\"model\":\"RDV2\",\"serial\":\"HD92731454\"},{\"id\":1,\"model\":\"DRV1\",\"serial\":\"HD78784271\"},{\"id\":2,\"model\":\"SSDLP2\",\"serial\":\"HD74286234\"},{\"id\":3,\"model\":\"SSDF1\",\"serial\":\"HD18368977\"},{\"id\":4,\"model\":\"123456789\",\"serial\":\"HD71847139\"},{\"id\":5,\"model\":\"HGST2048T\",\"serial\":\"HD60894593\"},{\"id\":6,\"model\":\"DRV1\",\"serial\":\"HD61514441\"},{\"id\":7,\"model\":\"DSD07461\",\"serial\":\"HD69247082\"},{\"id\":8,\"model\":\"broken\",\"serial\":\"HD30186042\"},{\"id\":9,\"model\":\"broken\",\"serial\":\"HD46171584\"},{\"id\":10,\"model\":\"broken\",\"serial\":\"HD96999858\"},{\"id\":11,\"model\":\"SSDDC1\",\"serial\":\"HD19987737\"},{\"id\":12,\"model\":\"RDV2\",\"serial\":\"HD23665165\"},{\"id\":13,\"model\":\"DRV1\",\"serial\":\"HD89836002\"},{\"id\":14,\"model\":\"SSDLP2\",\"serial\":\"HD71643186\"},{\"id\":15,\"model\":\"RDV2\",\"serial\":\"HD96068276\"},{\"id\":16,\"model\":\"HGST2048T\",\"serial\":\"HD55004428\"},{\"id\":17,\"model\":\"broken\",\"serial\":\"HD57141352\"},{\"id\":18,\"model\":\"SSDF1\",\"serial\":\"HD47013370\"},{\"id\":19,\"model\":\"SSDF1\",\"serial\":\"HD69336988\"}"};
-	uint32_t input_buffer_len = strlen(input_buffer);
-
-	char dst_buffer[16] = {};
-}
-*/
-
 int main(void) {
     UNITY_BEGIN();
 	RUN_TEST(test_json_find_begin);
